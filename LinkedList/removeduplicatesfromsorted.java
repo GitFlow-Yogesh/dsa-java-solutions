7ff971f035775bcd9db968fa364ca3198662e77d
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-05-18 01:44:49 +0530

    Create removeduplicatesfromsorted.java
    removing duplicates from sorted list

---
+++ LinkedList/removeduplicatesfromsorted.java
@@ -0,0 +1,14 @@
+public void removeduplicate(){
+        Node current =head;
+        while(current != null &&current.next != null){
+            if(current.data == current.next.data){
+                current.next=current.next.next;
+                size--;
+            }else{
+                current=current.next;
+            }
+          //updating tail just in case we remove last node which is duplicate
+            tail=current ;
+            tail.next=null;
+        }
+    }
