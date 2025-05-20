202f6e4e36e84574aaccc30d8b8c45b7428b58e0
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-05-21 00:58:33 +0530

    Update and rename checkingcyclicormiddlenode.java to checkingcyclic_or_middlenode_and_length.java
    find cycle , find middle , find length of cycle

--- LinkedList/checkingcyclic_or_middlenode_and_length.java
+++ LinkedList/checkingcyclic_or_middlenode_and_length.java
@@ -1,3 +1,4 @@
+//3 things = find cycle , find middle, find length of cycle =very basic thing 
 //very simple = it is called fast slow pointer technique 
 //used for = dectection cycle ,middle node =only using one traverse
 public boolean hasCycle(Node head) {
@@ -24,4 +25,24 @@ public Node findMiddle(Node head) {
     }
 
     return slow; // slow will be at the middle
+}
+
+//lets find out length of cycle 
+public int findlengthofcylce(Node head) {
+        Node slow = head;  
+        Node fast = head;  
+        while(fast != null && fast.next != null){
+            slow = slow.next;  
+            fast=fast.next.next; 
+            if(slow == fast){  //from here we will start counting
+                Node temp = slow;
+                int count =0;
+                temp = temp.next //or u can use do while loop same thing 
+                while(temp != fast){//u can compare with slow also same thing 
+                        temp = temp.next;
+                        count++;
+            }
+           return count;
+        }
+        return 0;   
 }
