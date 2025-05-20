38728e7758e30c014e298739a74b2f082d763a20
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-05-21 00:42:57 +0530

    Create checkingcyclicormiddlenode.java
    two question = first detect cycle , second find middle node using slow and fast pointer technique

---
+++ LinkedList/checkingcyclicormiddlenode.java
@@ -0,0 +1,27 @@
+//very simple = it is called fast slow pointer technique 
+//used for = dectection cycle ,middle node =only using one traverse
+public boolean hasCycle(Node head) {
+        Node slow = head;  //move one step
+        Node fast = head;  //move two step at a time 
+        while(fast != null && fast.next != null){
+            slow = slow.next; // one move 
+            fast=fast.next.next;   //moving twice 
+            if(slow == fast){  //checking if they meeting means cyclic 
+                return true;
+            }
+        }
+        return false;   
+}
+
+//checking middle node using slow and fast pointer technique only 
+public Node findMiddle(Node head) {
+    Node slow = head;
+    Node fast = head;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow; // slow will be at the middle
+}
