68215b821e84e02cc1be6dc2c390c18754cf6f4e
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-07-11 20:51:14 +0530

    Update rotation.java

--- arrays/basics/rotation.java
+++ arrays/basics/rotation.java
@@ -27,6 +27,7 @@ while (k > 0) {
     k--;
 }
 //more efficient way is using reverse algo bcz time is O(N) and space is O(1)== bro its very easy logic 
+//steps = first whole array , thn reverse first k , thn reverse remaing part 
 public static void rotateRight(int[] arr, int k) {
         int n = arr.length;
         if (n == 0 || k % n == 0) return;
@@ -55,3 +56,15 @@ public static void rotateRight(int[] arr, int k) {
     }
 
 //that's it buddy 
+
+
+//what if i want u to reverse by left .
+//same as above just reverse = first reverse first k element and thn reverse remaining elements and reverse the whole array = thats it other thing r same 
+// Step 1: Reverse first k elements
+        reverse(arr, 0, k - 1);
+
+        // Step 2: Reverse remaining elements
+        reverse(arr, k, n - 1);
+
+        // Step 3: Reverse the whole array
+        reverse(arr, 0, n - 1);
