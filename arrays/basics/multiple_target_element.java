8fb5afbfb3b86437ccb7d850edc0d5d6a740f352
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-07-11 21:15:22 +0530

    Create multiple_target_element.java

---
+++ arrays/basics/multiple_target_element.java
@@ -0,0 +1,13 @@
+//follow this = > Count = (lastIndex - firstIndex + 1)
+//make two function for first and for last occurence :
+//note all code is same as binary search 
+//for firstindex  change =
+        if (arr[mid] == target) {
+                result = mid;
+                high = mid - 1;  // keep looking on the left side
+            } 
+//for last index == jsut plus sign instead of negetive 
+        if (arr[mid] == target) {
+                result = mid;
+                high = mid + 1;  // keep looking on the left side
+            } 
