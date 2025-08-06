9d530d9f8cf75432a094f845a30b02351d5a6629
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-08-06 18:49:49 +0530

    Update bubble_sort.java

--- algorithm/bubble_sort.java
+++ algorithm/bubble_sort.java
@@ -1,4 +1,5 @@
- int[] arr = {3,2,5,1,6,8};
+//each iteration will take highest to the end thats it 
+int[] arr = {3,2,5,1,6,8};
         for(int i =0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]>arr[j+1]){
