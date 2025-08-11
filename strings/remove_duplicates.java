ce913bb3346c84ef5c3ccbdae73dd089b5e705a3
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-08-11 18:05:01 +0530

    Update remove_duplicates.java

--- strings/remove_duplicates.java
+++ strings/remove_duplicates.java
@@ -1,4 +1,5 @@
-//very simple just check if u have put the char in new string or not if not thn put elsse move to next step
+//very simple just check if u have put the char in new string or not if not thn put elsse move to next step , Time O(N^2)
+
       String str = "heelloll";
         String newstr="";
         for(int i=0;i<str.length();i++){
@@ -14,3 +15,16 @@
             }
         }
         System.out.print(newstr);
+
+///now lets use hashset to add only unique element which will reducce time complexity by O(N)
+String str = "heelloll";
+Set<Character> set = new HashSet<>();
+String newstr = "";
+
+for (char c : str.toCharArray()) {
+    if (!set.contains(c)) {
+        set.add(c);     // remember we saw it
+        newstr += c;    // put it in the result in correct order
+    }
+}
+System.out.println(newstr); // helo
