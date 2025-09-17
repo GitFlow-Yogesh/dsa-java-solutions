b227f94493d4e0fe3641506ab67dda8621211a34
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-09-17 23:43:50 +0530

    Create spiral_matrix.java

---
+++ 2d_array/spiral_matrix.java
@@ -0,0 +1,36 @@
+//ntg can be easier than this , just 2 min full concentration 
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if(matrix==null || matrix.length==0)return result;
+        int top =0;
+        int right = matrix[0].length-1;
+        int left =0;
+        int bottom = matrix.length-1;
+        while(left<=right && top <= bottom){
+            //step 1 left -> right
+            for(int i =left;i<=right;i++){
+                result.add(matrix[top][i]);
+            }
+            top++;
+            //step 2 top-> bottom 
+            for(int i=top;i<=bottom;i++){
+                result.add(matrix[i][right]);
+            }
+            right--;
+            //step 3 right->left
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            //step 4 bottom-> top
+            if(left<=right){
+                for(int i=bottom ;i>= top;i--){
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result ;
+    }
