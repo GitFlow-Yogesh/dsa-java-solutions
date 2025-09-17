1f198c55e792e39bf4847380fb565a0de216d89e
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-09-18 00:24:45 +0530

    Create set_matrix_zero_73.java

---
+++ 2d_array/set_matrix_zero_73.java
@@ -0,0 +1,23 @@
+  //first check where zero exist and store and thn change the data to zero 
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        // Step 1: Mark rows and columns
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        // Step 2: Update matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
