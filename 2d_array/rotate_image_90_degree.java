d73857308a483e4e25c3da0b90e274705c78ff24
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-09-17 23:17:32 +0530

    Create rotate_image_90_degree.java

---
+++ 2d_array/rotate_image_90_degree.java
@@ -0,0 +1,23 @@
+  //just two steps = first transpose and second reverse each row = very simple thats it 
+    public void rotate(int[][] matrix) {
+        //step 1 = transpose
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //step 2 = reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
